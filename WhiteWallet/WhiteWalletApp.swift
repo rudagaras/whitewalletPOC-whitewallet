7efca0cca1fa68ec20e88d1d3a59b83579161db9
Ruben Dario Garcia Astudillo <rg_e91446@osxglbusr02.local>
2023-04-17 17:12:17 -0500

    Starting point with targets configured

---
+++ WhiteWallet/WhiteWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiteWalletApp.swift
+//  WhiteWallet
+//
+//  Created by Ruben Dario Garcia Astudillo on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct WhiteWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
